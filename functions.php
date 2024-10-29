5ef97ea0c3c45d4b60e5889eb3e63d228327918c
Tom <tommywoodhouse02@gmail.com>
2024-10-28 22:44:45 -0400

    Added spacing-variables css

--- functions.php
+++ functions.php
@@ -1,3 +1,10 @@
 <?php
 require_once get_template_directory() . '/functions/generate-theme-variables.php';
+
+function enqueue_theme_assets() {
+    // Enqueue the static CSS file with hard-coded spacing variables
+    wp_enqueue_style('theme-spacing-variables', get_template_directory_uri() . '/styles/spacing-variables.css');
+}
+add_action('wp_enqueue_scripts', 'enqueue_theme_assets');
+
 ?>
