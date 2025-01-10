2ced8c047d02e5b8ea6fb34632b1f39374385fef
Tom <tommywoodhouse02@gmail.com>
2025-01-09 21:48:36 -0500

    Increased time to wait for iframe to prevent missing on page load

--- assets/ionicons/esm-loader.js
+++ assets/ionicons/esm-loader.js
@@ -1,9 +1,7 @@
 wp.domReady(() => {
   // Retry mechanism to find the iframe if it isn't ready immediately.
   const waitForIframe = () => {
-    const editorIframe = document.querySelector(
-      'iframe[title="Editor canvas"]',
-    ); // Update selector based on your inspection.
+    const editorIframe = document.querySelector('iframe[name="editor-canvas"]'); // Update selector based on your inspection.
 
     if (editorIframe) {
       // Check if the iframe is already loaded.
@@ -15,19 +13,20 @@ wp.domReady(() => {
       } else {
         // Add load event listener for the iframe.
         editorIframe.addEventListener("load", () => {
-          appendScriptToIframe(
-            editorIframe.contentDocument || editorIframe.contentWindow.document,
-          );
+          appendScriptToIframe(iframeDocument);
         });
       }
     } else {
-      setTimeout(waitForIframe, 100); // Retry after 100ms.
+      setTimeout(waitForIframe, 500); // Retry after 500ms.
     }
   };
 
   // Function to append the script to the iframe's body.
   const appendScriptToIframe = (iframeDocument) => {
-    if (iframeDocument) {
+    if (
+      iframeDocument &&
+      !iframeDocument.querySelector('script[src*="ionicons.esm.js"]')
+    ) {
       const script = iframeDocument.createElement("script");
       script.type = "module"; // Ensure the script is loaded as an ES Module.
       script.src = `${window.location.origin}/wp-content/themes/Rapid/assets/ionicons/ionicons/ionicons.esm.js?ver=7.4.0`; // URL to your ESM file.
