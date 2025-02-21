6b00c08ed61f5e3339151169219060fd21a7e7b5
Tom <tommywoodhouse02@gmail.com>
2025-02-20 21:43:11 -0500

    Fixed icon and label bugs

--- build/blocks/metric/render.php
+++ build/blocks/metric/render.php
@@ -5,9 +5,11 @@ $unit = $attributes["unit"];
 $size = $attributes["size"];
 $displayUpperLabel = $attributes["displayUpperLabel"];
 $upperLabel = $attributes["upperLabel"];
+$iconName = $attributes["iconName"];
 $tagName = "h" . $size;
 ?>
 <div <?php echo get_block_wrapper_attributes(); ?>>
+<ion-icon name="<?php echo esc_html($iconName); ?>"></ion-icon>
     <?php if ($displayUpperLabel && !empty($upperLabel)): ?>
         <p class="upper-label"><?php echo esc_html($upperLabel); ?></p>
     <?php endif; ?>
