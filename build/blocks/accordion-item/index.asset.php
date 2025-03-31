a30041e6e5be17c7931a6ec3045d84eaab17c960
Tom <tommywoodhouse02@gmail.com>
2025-03-31 18:04:03 -0400

    Mapped files

--- build/blocks/accordion-item/index.asset.php
+++ build/blocks/accordion-item/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'f8a03df99edc28e0556a');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '1e71c08992fd0e74223e');
--- build/blocks/accordion/index.asset.php
+++ build/blocks/accordion/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'caa5dee76a3841c70b88');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'a8c2921b58dcf3eb8ba0');
