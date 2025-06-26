034d78e00ef01629953d1023521d2ffb2c56d4b4
Tom <tommywoodhouse02@gmail.com>
2025-06-25 21:21:43 -0400

    Add parent block for Mega Menu Link block

--- build/blocks/mega-menu-link/index.asset.php
+++ build/blocks/mega-menu-link/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'ebf2252fbfcedbb7d55b');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'a6e898eb621a249f352c');
